ff562c7767c3e063dff5d29c32c598965c4b0cc1
Raj Kedia <rajramkola786@gmail.com>
2025-02-05 16:14:58 +0530

    Created the Chess reviewer

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'home/home.html')
+    # Create your views here.
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
