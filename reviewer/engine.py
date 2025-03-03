634621c1498ba2360ab9dc9f7cccf606ff3ff02d
Raj Kedia <rajramkola786@gmail.com>
2025-03-03 18:29:43 +0530

    added linux version of stockfish

--- reviewer/engine.py
+++ reviewer/engine.py
@@ -2,9 +2,17 @@ import chess
 import chess.pgn
 import chess.engine
 import os
+import platform
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-engine_path = os.path.join(BASE_DIR, 'staticfiles',
-                           'stockfish', 'stockfish-windows-x86-64-avx2.exe')
+if platform.system() == "Windows":
+    engine_path = os.path.join(
+        BASE_DIR, "staticfiles", "stockfish", "stockfish-windows-x86-64-avx2.exe")
+else:
+    engine_path = os.path.join(
+        BASE_DIR, "staticfiles", "stockfish", "stockfish-ubuntu-x86-64-avx2")
+# Normalize path (fix \ for Windows and / for Linux/Mac)
+STOCKFISH_PATH = os.path.normpath(engine_path)
 engine = None
 
 
