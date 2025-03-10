50e2f8d06b18d498ac08586b7aad5f1d4af3f528
Raj Kedia <rajramkola786@gmail.com>
2025-03-10 18:12:27 +0530

    change to public url for stockfish

--- reviewer/engine.py
+++ reviewer/engine.py
@@ -4,19 +4,12 @@ import chess.engine
 import os
 import platform
 import urllib.request
-
-# Define Cloud Storage bucket URL
-import os
-import platform
-import urllib.request
-
-BUCKET_URL = "https://storage.cloud.google.com/check-chess-game-review-system.appspot.com/"
+BUCKET_URL = "https://storage.googleapis.com/check-chess-game-review-system.appspot.com/"
 
 # Determine correct Stockfish file based on OS
-STOCKFISH_FILES = {
-    "Windows": "stockfish-windows-x86-64-avx2.exe",
-    "Linux": "stockfish-ubuntu-x86-64-sse41-popcnt"
-}
+STOCKFISH_FILES = ["stockfish-windows-x86-64-avx2.exe",
+                   "stockfish-ubuntu-x86-64-avx2"]
+
 PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOCAL_STOCKFISH_PATH = os.path.join(PARENT_DIR, 'staticfiles', "stockfish")
 
@@ -28,14 +21,15 @@ def is_running_in_cloud():
 
 def download_stockfish():
     """Use local Stockfish for development; download from GCS only in production."""
-    os_name = platform.system()
-    stockfish_file = STOCKFISH_FILES.get(os_name)
+    # os_name = platform.system()
+    # stockfish_file = STOCKFISH_FILES.get(os_name)
 
-    if not stockfish_file:
-        raise Exception("Unsupported OS for Stockfish")
+    # if not stockfish_file:
+    #     raise Exception("Unsupported OS for Stockfish")
 
     if not is_running_in_cloud():
         # Running locally, use the staticfiles directory
+        stockfish_file = STOCKFISH_FILES[0]
         local_path = os.path.join(LOCAL_STOCKFISH_PATH, stockfish_file)
         if os.path.exists(local_path):
             print(f"Using local Stockfish: {local_path}")
@@ -45,6 +39,7 @@ def download_stockfish():
                 f"Local Stockfish binary not found at {local_path}")
 
     # Running in cloud, use /tmp for App Engine
+    stockfish_file = STOCKFISH_FILES[1]
     cloud_path = os.path.join(BUCKET_URL, "/tmp", stockfish_file)
 
     if not os.path.exists(cloud_path):  # Download only if not already present
