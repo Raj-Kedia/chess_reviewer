91ac833984398b7762e2070e707244f9dd26929e
Raj Kedia <rajramkola786@gmail.com>
2025-07-25 13:19:24 +0530

    added echos in build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,12 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input 
 
+echo "✅ Static files collected successfully"
+
 # Run migrations
 python manage.py migrate --verbosity 2 --no-input
 
+echo "✅ Migrations applied successfully"
 # chmod +x /static/stockfish/stockfish-ubuntu-x86-64-avx2
 # chmod +x /staticfiles/stockfish/stockfish-ubuntu-x86-64-avx2
 echo "✅ Build script completed successfully"
